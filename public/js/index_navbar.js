6a6adb02dfea20a97a8718d8d1d996065ee453d7
MUKILAN S <mukilan069@gmail.com>
2022-11-12 10:50:03 +0530

    LandingPageFIx

---
+++ public/js/index_navbar.js
@@ -0,0 +1,36 @@
+var util = {
+    mobileMenu() {
+      $("#nav").toggleClass("nav-visible");
+    },
+    windowResize() {
+      if ($(window).width() > 800) {
+        $("#nav").removeClass("nav-visible");
+      }
+    },
+    scrollEvent() {
+      var scrollPosition = $(document).scrollTop();
+      
+      $.each(util.scrollMenuIds, function(i) {
+        var link = util.scrollMenuIds[i],
+            container = $(link).attr("href"),
+            containerOffset = $(container).offset().top,
+            containerHeight = $(container).outerHeight(),
+            containerBottom = containerOffset + containerHeight;
+  
+        if (scrollPosition < containerBottom - 20 && scrollPosition >= containerOffset - 20) {
+          $(link).addClass("active");
+        } else {
+          $(link).removeClass("active");
+        }
+      });
+    }
+  };
+  
+  $(document).ready(function() {
+    
+    util.scrollMenuIds = $("a.nav-link[href]");
+    $("#menu").click(util.mobileMenu);
+    $(window).resize(util.windowResize);
+    $(document).scroll(util.scrollEvent);
+  });
+  
